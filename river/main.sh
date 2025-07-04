8a8af05cf6d461bf7fcbdaadae1cfeab70990ae1
Thanh-Giang (River) Tan Nguyen <nttg8100@gmail.com>
2025-07-05 01:59:34 +0700

    refactor env

--- river/main.sh
+++ river/main.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Add this to allow load the variables form config json
-while IFS== read key value; do
-    printf -v "$key" "$value"
-done < <(jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' config.json)
-
-
 echo ""
 echo "============================================="
 echo "          RIVER ANALYSIS TEMPLATE"
@@ -14,10 +8,10 @@ echo ""
 # Default Environment Variables
 echo ">> Default Environment Variables:"
 echo "---------------------------------------------"
-echo "UUID Job ID  : $uuid_job_id"
-echo "CPUs         : $cpus"
+echo "UUID Job ID  : $job_id"
+echo "CPU          : $cpu"
 echo "Memory       : $memory"
-echo "Times        : $times"
+echo "Time         : $time"
 echo "RIVER_HOME   : $RIVER_HOME"
 echo "---------------------------------------------"
 echo ""
@@ -39,13 +33,13 @@ echo ">> Bootstrap: Uncomment if you want to use custom reverse proxy"
 echo "---------------------------------------------"
 # Custom reverse location.
 # Example: RStudio Server automatically redirects to `/` and restricts iframe usage.
-echo "$uuid_job_id/" > "$RIVER_HOME/.river/jobs/$uuid_job_id/job.proxy_location"
+echo "$job_id/" > "$RIVER_HOME/jobs/$job_id/job.proxy_location"
 
 # Example: VS Code Server or JupyterLab requires reverse proxy.
-echo "$uuid_job_id" > "$RIVER_HOME/.river/jobs/$uuid_job_id/job.url"
+echo "$job_id" > "$RIVER_HOME/jobs/$job_id/job.url"
 
 # If the job is only exported via port (basic web app), still create this file but leave it empty.
-touch "$RIVER_HOME/.river/jobs/$uuid_job_id/job.url"
+touch "$RIVER_HOME/jobs/$job_id/job.url"
 echo "---------------------------------------------"
 echo ""
 
